e6798ccd4cded58579fa180b6afbccc41b6305ee
publizm <publizm@gmail.com>
2019-12-15 11:23:45 +0900

    docs: complete programmers17

---
+++ programmers17.js
@@ -0,0 +1,26 @@
+// 자연수 뒤집어 배열로 만들기
+
+// 문제 설명
+// 자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+
+// 제한사항
+// n은 10,000,000,000이하인 자연수입니다.
+
+
+// 입출력 예
+// n
+// 12345
+
+// return
+// [5,4,3,2,1]
+
+function solution(n) {
+  const result = [];
+  const arr = ('' + n).split('').reverse();
+
+  for (let i = 0; i < arr.length; i++) {
+    result.push(+arr[i]);
+  }
+
+  return result;
+}
