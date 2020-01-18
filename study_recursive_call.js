d71e93b8592895f421887ae0810a0d11fb4098c5
publizm <publizm@gmail.com>
2020-01-18 12:27:25 +0900

    docs: study recursive call

--- study_recursive_call.js
+++ study_recursive_call.js
@@ -2,7 +2,7 @@
 
 // 재귀 용법이란?
 // 함수 안에서 동일한 함수를 호출하는 형태
-// 주로, 재귀호출하며 함수의 인수에 숫자를 줄여준다든지, 배열의 길이를 줄인다는지의 방법을 사용
+// 주로, 재귀호출하며 함수의 인수에 숫자를 줄여준다든지, 배열의 길이를 줄인다는지의 방법을 사용하여 원하는 결과값을 도출
 
 // 중복없는 난수 배열 만드는 함수
 function randomArray(array, length) {
@@ -18,7 +18,8 @@ function randomArray(array, length) {
 
 // 예제 1
 // 1부터 num까지의 곱이 출력되게 만들어라
-function solution(num) {
+
+function multiple(num) {
   // 반복문을 활용
   // let result = 1;
   // for (let i = num; i > 0; i--) {
@@ -27,21 +28,21 @@ function solution(num) {
   // return result;
 
   // 재귀함수 활용
-  if (num > 1) return num * solution(num - 1);
+  if (num > 1) return num * multiple(num - 1);
   return num;
 }
 
-console.log(solution(10));
+console.log(multiple(10));
 
 // 예제2
 // 숫자가 들어 있는 배열이 주어졌을때, 리스트의 합을 리턴하는 함수를 만들어라
-function solution2(arr) {
+function sum(arr) {
   console.log(arr);
-  if (arr.length > 1) return arr[0] + solution2(arr.slice(1));
+  if (arr.length > 1) return arr[0] + sum(arr.slice(1));
   return arr[0];
 }
 
-console.log(solution2(randomArray([], 10)));
+console.log(sum(randomArray([], 10)));
 
 // 예제3
 // 순서를 거꾸로 읽어도 제대로 읽는 것과 같은 단어와 문장(회문)을 판별할 수 있는 함수를 만드세요
@@ -58,10 +59,25 @@ console.log(solution2(randomArray([], 10)));
 // }
 
 // 디벨롭
-function solution3(str) {
+function palindrome(str) {
   if (str.length <= 1) return true;
-  if (str[0] === str[str.length - 1]) return solution3(str.slice(1, -1));
+  if (str[0] === str[str.length - 1]) return palindrome(str.slice(1, -1));
   return false;
 }
 
-console.log(solution3('level'));
+console.log(palindrome('level'));
+
+// 예제4
+// 정수 n에 대해 n이 홀수이면 3 x n + 1을 하고, 짝수이면 n을 2로 나눠 n이 1이 될 때까지 반복합니다.
+// 위의 과정에 n이 1이 되는 과정을 모두 출력하는 함수를 만드세요
+
+function processing(n) {
+  console.log(n);
+
+  if (n === 1) return n;
+
+  if (n % 2) return processing(3 * n + 1);
+  else return processing(n / 2);
+}
+
+processing(3);
